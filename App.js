7282ce8714d0ecfb319ad7168ef4ade2a7cf9a29
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2023-07-22 20:13:43 -0300

    add button e display

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+// Necessário, pois o Expo tem o Input dentro do node_modules e o .gitignore volta para a configuração inicial
+import Initial from "./src/App";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Initial />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
