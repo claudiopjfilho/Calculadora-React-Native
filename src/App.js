659d3af3fe34e05cc3fd4e36bef4bc61390744f2
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2023-07-25 23:21:56 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
     }
 
     const clearDisplay =
-      this.state.displayValue === "0" || this.state.clearDisplays;
+      this.state.displayValue === "0" || this.state.clearDisplay;
     const currentValue = clearDisplay ? "" : this.state.displayValue;
 
     const displayValue = currentValue + n;
@@ -41,7 +41,28 @@ export default class App extends Component {
     this.setState({ ...initialState });
   };
 
-  setOperation = operation => {};
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`); //Vai realizar toda a operação, exemplo: 2 + 3 = 5
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
+  };
 
   render() {
     return (
